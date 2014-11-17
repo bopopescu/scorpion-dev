2491caef60fd7e571f166f656ba3aa0af829a7bc
Brett Wilcox <brett@brettwilcox.com>
2014-11-16 21:27:40 -0600

    Playing with installer.

---
+++ install/scorpion-install.py
@@ -0,0 +1,59 @@
+#################################################################
+##
+##        Scorpion Server Manager
+##
+##             (        )
+##             O        O
+##             ()      ()
+##              Oo.nn.oO
+##               _mmmm_
+##             \/_mmmm_\/
+##             \/_mmmm_\/
+##             \/_mmmm_\/
+##             \/ mmmm \/
+##                 nn
+##                 ()   scorpion.io
+##                 ()
+##                  ()    /
+##                   ()__()
+##                    '--'
+##
+## Copyright (c) 2014 the scorpion.io authors. All rights reserved.
+## Use of this source code is governed by a MIT license that can be
+## found in the LICENSE file.
+##
+#################################################################
+
+import os
+import argparse
+
+try: input = raw_input
+except NameError: pass
+
+parser = argparse.ArgumentParser(description='AWS Credentials for saving and restoring backups.')
+parser.add_argument('--aws-access-key-id')
+parser.add_argument('--aws-secret-access-key')
+
+args = parser.parse_args()
+
+AWS_AKID = args.aws_access_key_id
+AWS_SAK = args.aws_secret_access_key
+
+if not AWS_AKID:
+    AWS_AKID = input('Enter your AWS Access Key ID: ')
+
+if not AWS_SAK:
+    AWS_SAK = input('Enter your AWS Secret Access Key: ')
+
+print AWS_AKID
+print AWS_SAK
+
+## Kick off another script
+#os.system('python hello.py')
+
+print('Complete')
+
+try:
+    os.remove(scorpion-install.py)
+except OSError:
+    pass
--- scorpion.py
+++ scorpion.py
@@ -1,43 +1,46 @@
-#
-#        Scorpion Server Manager
-#
-#             (        )
-#             O        O
-#             ()      ()
-#              Oo.nn.oO
-#               _mmmm_
-#             \/_mmmm_\/
-#             \/_mmmm_\/
-#             \/_mmmm_\/
-#             \/ mmmm \/
-#                 nn
-#                 ()   scorpion.io
-#                 ()
-#                  ()    /
-#                   ()__()
-#                    '--'
-#
-# Copyright (c) 2014 the scorpion authors. All rights reserved.
-# Use of this source code is governed by a MIT license that can be
-# found in the LICENSE file.
+#################################################################
+##
+##        Scorpion Server Manager
+##
+##             (        )
+##             O        O
+##             ()      ()
+##              Oo.nn.oO
+##               _mmmm_
+##             \/_mmmm_\/
+##             \/_mmmm_\/
+##             \/_mmmm_\/
+##             \/ mmmm \/
+##                 nn
+##                 ()   scorpion.io
+##                 ()
+##                  ()    /
+##                   ()__()
+##                    '--'
+##
+## Copyright (c) 2014 the scorpion.io authors. All rights reserved.
+## Use of this source code is governed by a MIT license that can be
+## found in the LICENSE file.
+##
+#################################################################
 
-# Allow modules to be imported from the libraries directory.
+## Allow modules to be imported from the libraries directory.
 import sys
 sys.path.append('./libraries')
 
-# Import modules for use
+## Import modules for use
 import os
 import platform
 
-# Maybe someday I will create an interface using blessings...
-# from blessings import Terminal
+## Maybe someday I will create an interface using blessings...
+#from blessings import Terminal
 
 os.environ["AWS_CREDENTIAL_FILE"] = ".awskeys"
 
-# Allow the script to move forward regardless of compatibility issues.
-devmode = False
+## Allow the script to move forward regardless of compatibility issues.
+devmode = True
 
-# Check if dev mode or not.
+## Check if dev mode or not.
 if not devmode:
     # Check if Python is > 2.7
     if sys.version_info[:2] < (2, 7):
@@ -51,7 +54,7 @@ if not devmode:
         print('#' * 65)
         sys.exit(1)
 
-    # Check if windows
+    ## Check if windows
     if platform.system() == 'Windows':
         print('#' * 65)
         print '''
@@ -63,15 +66,28 @@ if not devmode:
         print('#' * 65)
         sys.exit(1)
 
-# Create connection to AWS S3
+    ## Check if Mac
+    if platform.system() == 'Darwin':
+        print('#' * 65)
+        print '''
+        scorpion does not support OS X
+
+        We have validated that scorpion works on the following systems
+           * Ubuntu 14.04
+        '''
+        print('#' * 65)
+        sys.exit(1)
+
+## Uncheck to see distribution information.
+#print platform.dist()
+#print platform.system()
+
+## Create connection to AWS S3
 #s3conn = boto.connect_s3()
 
-# Get all available buckets.
+## Get all available buckets.
 #buckets = s3conn.get_all_buckets()
 
-# List the buckets.
+## List the buckets.
 #for bucket in buckets:
 #    print bucket.name
-
-print platform.dist()
-print platform.system()
