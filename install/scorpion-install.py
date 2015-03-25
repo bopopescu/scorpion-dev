ca692bafe5d18ef744135d05790563bc055e691f
Brett Wilcox <brett@brettwilcox.com>
2015-03-25 09:23:18 -0500

    Updating with install messaging

--- install/scorpion-install.py
+++ install/scorpion-install.py
@@ -42,27 +42,28 @@ except NameError: pass
 devmode = True
 
 # Update OS
-os_update = False
+os_update = True
 os_upgrade = False
 
 # Set Hostname
-set_hostname = False
+set_hostname = True
 
 # Networking
-set_ipv4 = False
-set_ipv6 = False
-set_localhost = False
-restart_net_interfaces = False
+set_ipv4 = True
+set_ipv6 = True
+set_localhost = True
+restart_net_interfaces = True
 
 # Software Installs
-scorpion_install = False
-postfix_install = False
-mysql_install = False
-php_install = False
-nginx_install = False
-supporting_software_install = False
+scorpion_install = True
+postfix_install = True
+mysql_install = True
+php_install = True
+nginx_install = True
+supporting_software_install = True
 gitlab_install = False
 s3cmd_install = True
+apache_install = True
 
 
 #################################################################
@@ -134,6 +135,21 @@ if not devmode:
 
 
 #################################################################
+# Functions used in script
+#################################################################
+
+def installMessageStart(installMessage):
+    print('*' * 65)
+    print " %s" % installMessage
+    print ""
+
+def installMessageEnd():
+    print " Done"
+    print('*' * 65)
+    print ""
+
+
+#################################################################
 # Command line arguments
 #################################################################
 
@@ -183,12 +199,20 @@ if not PASSWORD:
 #################################################################
 
 if os_update:
+    installMessageStart("Starting apt-get Update")
+
     # Update the packages repo
-    os.system("sudo apt-get update")
+    os.system("sudo apt-get -qq -y update")
+
+    installMessageEnd()
 
 if os_upgrade:
+    installMessageStart("Updating OS")
+
     # Upgrade installed packages to latest versions
-    os.system("sudo apt-get upgrade -y")
+    os.system("sudo apt-get -qq -y upgrade")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -196,9 +220,13 @@ if os_upgrade:
 #################################################################
 
 if set_hostname:
+    installMessageStart("Updating Hostname")
+
     hostnameupdatestring = "echo %s > /etc/hostname" % HOSTNAME
     os.system(hostnameupdatestring)
     os.system("hostname -F /etc/hostname")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -207,6 +235,8 @@ if set_hostname:
 
 # Set the IPv4 Address
 if set_ipv4:
+    installMessageStart("Setting IPv4 Network Address")
+
     print('*' * 65)
     print " Setting IPv4 Address"
     get_ipv4 = "ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://'"
@@ -217,8 +247,12 @@ if set_ipv4:
     print " Set IPv4 to %s" % ipv4address
     print('*' * 65)
 
+    installMessageEnd()
+
 # Set the IPv6 Address
 if set_ipv6:
+    installMessageStart("Setting IPv6 Network Address")
+
     print('*' * 65)
     print " Setting IPv6 Address"
     get_ipv6 = "ifconfig eth0 | awk '/inet6 / { print $3;exit; }' | sed 's/\/64// '"
@@ -229,8 +263,12 @@ if set_ipv6:
     print " Set IPv6 to %s" % ipv6address
     print('*' * 65)
 
+    installMessageEnd()
+
 # Set the localhost Address
 if set_localhost:
+    installMessageStart("Setting localhost Network Address")
+
     print('*' * 65)
     print " Setting localhost Address"
     set_localhost_command = "echo 127.0.0.1 %s >> /etc/hosts" % HOSTNAME
@@ -238,13 +276,19 @@ if set_localhost:
     print " Set localhost with hostname %s." % HOSTNAME
     print('*' * 65)
 
+    installMessageEnd()
+
 # Restart network interfaces
 if restart_net_interfaces:
+    installMessageStart("Restarting Network Interfaces")`
+
     print('*' * 65)
     print " Setting localhost Address"
     os.system("ifdown -a")
     os.system("ifup -a")
     os.system("ifup eth0")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -253,15 +297,24 @@ if restart_net_interfaces:
 
 # Install scorpion requirements
 if not os.path.isdir("/scorpion"):
+    installMessageStart("Installing Git and downloading Scorpion Core")
+
     # Install Git to version control scorpion core
-    os.system("apt-get -y install git")
+    os.system("apt-get -qq -y install git")
     # Make the directory scorpion will reside
     os.makedirs("/scorpion")
     # Git clone the core
     os.system("git clone https://github.com/scorpion/scorpion-dev.git /scorpion")
+
+    installMessageEnd()
+
 else:
+    installMessageStart("Scorpion installed, updating core to latest version")
+
     # Folder exists, so just update
     os.system("git -C /scorpion pull")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -269,14 +322,18 @@ else:
 #################################################################
 
 if postfix_install:
+    installMessageStart("Installing Postfix")
+
     # Prestage postfix questions with default answers
     os.system("echo 'postfix postfix/main_mailer_type select Internet Site' | debconf-set-selections")
     os.system("echo 'postfix postfix/mailname string localhost' | debconf-set-selections")
     os.system("echo 'postfix postfix/destinations string localhost.localdomain, localhost' | debconf-set-selections")
 
     # Kick off the install
-    os.system("apt-get -y install postfix")
+    os.system("apt-get -qq -y install postfix")
     os.system("/usr/sbin/postconf -e 'inet_interfaces = loopback-only'")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -284,15 +341,16 @@ if postfix_install:
 #################################################################
 
 if mysql_install:
+    installMessageStart("Installing MySQL")
+
     # Make installer non-interactive
-    #os.system("export DEBIAN_FRONTEND=noninteractive")
     mysql_pass_command1 = "echo 'mysql-server mysql-server/root_password password %s' | sudo debconf-set-selections" % PASSWORD
     mysql_pass_command2 = "echo 'mysql-server mysql-server/root_password_again password %s' | sudo debconf-set-selections" % PASSWORD
     os.system(mysql_pass_command1)
     os.system(mysql_pass_command2)
 
     # Install MySQL
-    os.system("apt-get -q -y install mysql-server")
+    os.system("apt-get -qq -y install mysql-server")
 
     # Set MySQL Password
     mysql_password_set = "mysqladmin -u root password %s" % PASSWORD
@@ -302,14 +360,18 @@ if mysql_install:
     mysql_install_secure = "sudo sh /scorpion/install/mysql_secure.sh %s" % PASSWORD
     os.system(mysql_install_secure)
 
+    installMessageEnd()
+
 
 #################################################################
 # PHP Install
 #################################################################
 
 if php_install:
+    installMessageStart("Installing PHP")
+
     # Install PHP
-    os.system("apt-get -y install php5-fpm php5-cli php5-curl php5-gd php5-mcrypt php5-mysql php5-sqlite php-apc php-pear php5-tidy php5-imap")
+    os.system("apt-get -qq -y install php5-fpm php5-cli php5-curl php5-gd php5-mcrypt php5-mysql php5-sqlite php-apc php-pear php5-tidy php5-imap")
 
     # Fix PHP5-IMAP extension by creating symbolic link
     os.system("sudo ln -s ../../mods-available/imap.ini /etc/php5/fpm/conf.d/20-imap.ini")
@@ -317,13 +379,33 @@ if php_install:
     # Fix 502 Bad Gateway
     #sed -i 's@listen = /var/run/php5-fpm.sock@listen = 127.0.0.1:9000@g' /etc/php5/fpm/pool.d/www.conf
 
+    installMessageEnd()
+
 
 #################################################################
 # NGINX Install
 #################################################################
 
 if nginx_install:
-    os.system("apt-get -q -y install nginx")
+    installMessageStart("Installing PHP")
+
+    # Installo Nginx
+    os.system("apt-get -qq -y install nginx")
+
+    installMessageEnd()
+
+
+#################################################################
+# Apache Install
+#################################################################
+
+if apache_install:
+    installMessageStart("Installing Apache")
+
+    # Install Apache
+    os.system("apt-get -qq -y install apache")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -331,7 +413,12 @@ if nginx_install:
 #################################################################
 
 if supporting_software_install:
-    os.system("apt-get -q -y install htop imagemagick iftop mytop iptraf nmon lynx nmap screen monit mutt")
+    installMessageStart("Installing Supporting Software")
+
+    # Install Supporting Software
+    os.system("apt-get -qq -y install htop imagemagick iftop mytop iptraf nmon lynx nmap screen monit mutt")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -340,13 +427,20 @@ if supporting_software_install:
 
 # Begin Gitlab install
 if gitlab_install:
-    os.system("sudo apt-get -y install openssh-server")
-    os.system("sudo apt-get -y install postfix")
+    installMessageStart("Installing Supporting Software")
 
+    # Install required pre-requisites
+    os.system("apt-get -qq -y install openssh-server")
+    os.system("apt-get -qq -y install postfix")
+
+    # Download and install Gitlab
     os.system("wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.8.4-omnibus-1_amd64.deb")
     os.system("sudo dpkg -i gitlab_7.8.4-omnibus-1_amd64.deb")
 
+    # Configure Gitlab
     os.system("sudo gitlab-ctl reconfigure")
+
+    installMessageEnd()
 
 
 #################################################################
@@ -354,19 +448,27 @@ if gitlab_install:
 #################################################################
 
 if s3cmd_install:
+    installMessageStart("Installing S3CMD")
+
     # Install S3CMD
-    os.system("apt-get -q -y install s3cmd")
+    os.system("apt-get -qq -y install s3cmd")
 
     # Set MySQL Password
     s3cmd_setup_command = "sudo sh /scorpion/install/s3cmd_config.sh %s %s" % (AWS_AKID, AWS_SAK)
     os.system(s3cmd_setup_command)
+
+    installMessageEnd()
 
 
 #################################################################
 # Post Install Cleanup
 #################################################################
 
-print('Complete')
+print ""
+print ""
+print ""
+print ""
+print('Scorpion Install Complete!')
 
 try:
     os.remove('scorpion-install.py')
